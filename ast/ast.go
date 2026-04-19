f52a85bb283da1123e4bb31a8cb2a6cc08ff6df2
maelho <51026438+maelho@users.noreply.github.com>
2026-04-19 18:03:17 -0400

    wip-parser

---
+++ ast/ast.go
@@ -0,0 +1,30 @@
+// Package ast
+package ast
+
+// let <identifier> = <expression>;
+
+type Node interface {
+	TokenLiteral() string
+}
+
+type Statement interface {
+	Node
+	statementNode()
+}
+
+type Expression interface {
+	Node
+	expressionNode()
+}
+
+type Program struct {
+	Statement []Statement
+}
+
+func (p *Program) TokenLiteral() string {
+	if len(p.Statement) > 0 {
+		return p.Statement[0].TokenLiteral()
+	} else {
+		return ""
+	}
+}
