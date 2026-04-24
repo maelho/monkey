7e85f69f1a6ee624ef508900bb81a3e9d9b50ec7
maelho <51026438+maelho@users.noreply.github.com>
2026-04-24 09:51:42 -0400

    evaluate expressions

---
+++ evaluator/evaluator.go
@@ -0,0 +1,34 @@
+// Package evaluator
+package evaluator
+
+import (
+	"maelho.github.io/monkey/ast"
+	"maelho.github.io/monkey/object"
+)
+
+func Eval(node ast.Node) object.Object {
+	switch node := node.(type) {
+	// Stataments
+	case *ast.Program:
+		return evalStataments(node.Statements)
+
+	case *ast.ExpressionStatement:
+		return Eval(node.Expression)
+
+	// Expressions
+	case *ast.IntegerLiteral:
+		return &object.Integer{Value: node.Value}
+	}
+
+	return nil
+}
+
+func evalStataments(stmts []ast.Statement) object.Object {
+	var result object.Object
+
+	for _, statement := range stmts {
+		result = Eval(statement)
+	}
+
+	return result
+}
--- object/object.go
+++ object/object.go
@@ -20,17 +20,17 @@ type Integer struct {
 	Value int64
 }
 
-func (i *Integer) Type() string    { return INTEGER_OBJ }
-func (i *Integer) Inspect() string { return fmt.Sprintf("%d", i.Value) }
+func (i *Integer) Type() ObjectType { return INTEGER_OBJ }
+func (i *Integer) Inspect() string  { return fmt.Sprintf("%d", i.Value) }
 
 type Boolean struct {
 	Value bool
 }
 
-func (i *Boolean) Type() string    { return BOOLEAN_OBJ }
-func (i *Boolean) Inspect() string { return fmt.Sprintf("%t", i.Value) }
+func (i *Boolean) Type() ObjectType { return BOOLEAN_OBJ }
+func (i *Boolean) Inspect() string  { return fmt.Sprintf("%t", i.Value) }
 
 type Null struct{}
 
-func (i *Null) Type() string    { return NULL_OBJ }
-func (i *Null) Inspect() string { return "null" }
+func (i *Null) Type() ObjectType { return NULL_OBJ }
+func (i *Null) Inspect() string  { return "null" }
